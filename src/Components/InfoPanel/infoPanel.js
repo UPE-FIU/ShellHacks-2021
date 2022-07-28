1b8d537cde2f5d96473d9ae6bc67e00341c078b6
apthos <david00lara@gmail.com>
2022-07-27 22:35:43 -0700

    Updates for 2022

--- src/Components/InfoPanel/infoPanel.js
+++ src/Components/InfoPanel/infoPanel.js
@@ -1,26 +1,38 @@
-import React, { Component } from 'react';
-import './infoPanel.css'
-import ShellHacksLogo from '../../SVG/SH_Logo_Pink_Slogan.svg'
+import React, { Component } from "react";
+import "./infoPanel.css";
+import ShellHacksLogo from "../../SVG/SH_Logo_Pink_Slogan.svg";
 
 class infoPanel extends Component {
     render() {
         return (
             <div className="infoContainer">
                 <div className="shellLogo">
-                    <img src={ShellHacksLogo} alt='ShellHacks Logo'  />
+                    <img src={ShellHacksLogo} alt="ShellHacks Logo" />
                 </div>
                 {/* <h1>Florida's Largest Hackathon</h1> */}
                 <h2 className="subheading">September 24th - 26th, 2021</h2>
                 <h2 className="subheading">Virtual • FIU, Miami FL</h2>
                 <div className="buttonsDiv">
-                    <a href="https://airtable.com/shrw0Nxg4xHaIGjSC" className="registerButton">Register Now!</a>
+                    <a href="https://shellhacks.net" className="registerButton">
+                        Register Now!
+                    </a>
                     <div className="buttonRow">
-                        <a href="https://discord.gg/upefiu" className="rowButton">Join Discord</a>
-                        <a href="mailto:Upe@fiu.edu?subject=We want to sponsor" className="rowButton">Sponsor Us</a>
+                        <a
+                            href="https://discord.gg/upefiu"
+                            className="rowButton"
+                        >
+                            Join Discord
+                        </a>
+                        <a
+                            href="mailto:Upe@fiu.edu?subject=We want to sponsor"
+                            className="rowButton"
+                        >
+                            Sponsor Us
+                        </a>
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 }
 
